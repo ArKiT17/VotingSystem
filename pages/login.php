430ec631046f247c3e294c909b30e1b391612559
Макс Лик <maxlikarenko@gmail.com>
2024-03-14 23:47:27 +0100

    - стилізовані сторінки реєстрації та входу в акаунт

--- pages/login.php
+++ pages/login.php
@@ -5,34 +5,42 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Вхід в акаунт</title>
+    <link href="https://fonts.googleapis.com/css2?family=Convergence&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+          rel="stylesheet">
     <link rel="stylesheet" href="../css/registerLogin.css">
+    <title>Вхід в акаунт</title>
 </head>
 <body>
-<div class="login">
-    <h1>Вхід</h1>
-    <h2>
-        <?php
-        session_start();
-        if ($_SESSION['login'] !== null)
-            header("Location: ../pages/availableVotes.php");
-        const errors = array(
-            'invalid_user' => 'Користувача не знайдено. Будь ласка, <a href="register.php">зареєструйтесь</a>.',
-            'incorrect_password' => 'Невірний пароль.'
-        );
-        echo(errors[$_GET['error']]);
-        ?>
-    </h2>
-    <form action="../php/login.php" method="post">
-        <label for="input-login">Логін:</label>
-        <input id="input-login" type="text" name="login" required oninput="checkInput()">
-        <label for="input-password">Пароль:</label>
-        <input id="input-password" type="password" name="password" required oninput="checkInput()">
-        <input id="t" type="hidden" name="t">
-        <button name="btn" id="submit-login" type="submit" disabled>Login</button>
-    </form>
-    <a href="register.php">Ще не маєш акаунту? Зареєструватися.</a>
-</div>
+<header>
+    <h1 class="title">Вхід</h1>
+</header>
+<main>
+    <div class="form">
+        <h2>
+            <?php
+            session_start();
+            if ($_SESSION['login'] !== null)
+                header("Location: ../pages/availableVotes.php");
+            const errors = array(
+                'invalid_user' => 'Користувача не знайдено.</br>Будь ласка, <a class="link" href="register.php">зареєструйтесь</a>.',
+                'incorrect_password' => 'Невірний пароль.'
+            );
+            echo(errors[$_GET['error']]);
+            ?>
+        </h2>
+        <form action="../php/login.php" method="post">
+            <label for="input-login">Логін</label>
+            <input id="input-login" type="text" name="login" required oninput="checkInput()">
+            <p></p>
+            <label for="input-password">Пароль</label>
+            <input id="input-password" type="password" name="password" required oninput="checkInput()">
+            <p></p>
+            <input id="t" type="hidden" name="t">
+            <button name="btn" id="submit-login" type="submit" disabled>Login</button>
+        </form>
+        <a class="link" href="register.php">Ще не маєш акаунту? Зареєструватися.</a>
+    </div>
+</main>
 <?php include "./components/footer.php" ?>
 <script>
     const button = document.getElementById('submit-login')
@@ -44,7 +52,7 @@
             inputPassword.value !== '');
     }
 
-    document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', function () {
         const date = new Date;
         document.getElementById('t').value = date.getTimezoneOffset() / 60
     });
--- pages/register.php
+++ pages/register.php
@@ -5,35 +5,42 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Реєстрація</title>
+    <link href="https://fonts.googleapis.com/css2?family=Convergence&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+          rel="stylesheet">
     <link rel="stylesheet" href="../css/registerLogin.css">
+    <title>Реєстрація</title>
 </head>
 <body>
-<div class="register">
-    <h1>Реєстрація</h1>
-    <h2>
-        <?php
-        if ($_GET['error']) {
-            echo('Користувач вже існує. Будь ласка, <a href="login.php">увійдіть в акаунт</a>.');
-        }
-        ?>
-    </h2>
-    <form name="registerForm" action="../php/register.php" method="post">
-        <label for="input-login">Логін:</label>
-        <input id="input-login" type="text" name="login" required oninput="checkInput()">
-        <p id="errorLogin"></p>
-        <label for="input-password">Пароль:</label>
-        <input id="input-password" type="password" name="password" required oninput="checkInput()">
-        <label for="input-password-confirm">Підтвердження пароля:</label>
-        <input id="input-password-confirm" type="password" name="passwordConfirm" required oninput="checkInput()">
-        <p id="errorPass"></p>
-        <label for="input-name">Ім'я та прізвище:</label>
-        <input id="input-name" type="text" name="name" required oninput="checkInput()">
-        <p id="errorName"></p>
-        <button id="submit-register" type="button" onclick="validation()" disabled>Зареєструватися</button>
-    </form>
-    <a class="link" href="login.php">Вже маєш акаунт? Увійти.</a>
-</div>
+<header>
+    <h1 class="title">Реєстрація</h1>
+</header>
+<main>
+    <div class="form">
+        <h2>
+            <?php
+            if ($_GET['error']) {
+                echo('Користувач вже існує.</br>Будь ласка, <a class="link" href="login.php">увійдіть в акаунт</a>.');
+            }
+            ?>
+        </h2>
+        <form name="registerForm" action="../php/register.php" method="post">
+            <label for="input-login">Логін</label>
+            <input id="input-login" type="text" name="login" required oninput="checkInput()">
+            <p id="errorLogin"></p>
+            <label for="input-password">Пароль</label>
+            <input id="input-password" type="password" name="password" required oninput="checkInput()">
+            <p></p>
+            <label for="input-password-confirm">Повторіть пароль</label>
+            <input id="input-password-confirm" type="password" name="passwordConfirm" required oninput="checkInput()">
+            <p id="errorPass"></p>
+            <label for="input-name">Ім'я та прізвище</label>
+            <input id="input-name" type="text" name="name" required oninput="checkInput()">
+            <p id="errorName"></p>
+            <button id="submit-register" type="button" onclick="validation()" disabled>Зареєструватися</button>
+        </form>
+        <a class="link" href="login.php">Вже маєш акаунт? Увійти.</a>
+    </div>
+</main>
 <?php include "./components/footer.php" ?>
 <script>
     const button = document.getElementById('submit-register')
